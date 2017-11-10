5a7d9c6ab50d40fa920855fea7b713e2a24b0f0b
OlBesp <olbesp365@gmail.com>
2017-11-10 15:31:09 +0200

    Add smooth scrolling for menu items

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  var menuBtn = document.querySelector('#navi-toggle');
+  var menuItemsList = document.querySelectorAll('.navigation__link');
+  var sectionList = document.querySelectorAll('section');
+
+  for (var i = 0; i < menuItemsList.length; i++) {
+    menuItemsList[i].addEventListener('click', scroll);
+  }
+
+  function scroll(event) {
+    // Close menu
+    menuBtn.checked = false;
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  }
+
+
+});
